42635a6ddc37171e7bdb041c62e9300c7bc9423d
jbzion <eric11jhou@gmail.com>
2022-12-23 17:10:24 +0800

    ok

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
@@ -23,14 +22,9 @@ type User struct {
 	OutMin      int
 }
 
-var subscribeMap map[string]map[string]*User // map[groupID][userID]*User
-
-func init() {
-	subscribeMap = make(map[string]map[string]*User)
-	ok = make(map[string]map[string]time.Time)
-}
-
 var bot *linebot.Client
+
+const chatGptURL = "https://api.openai.com/v1/chat/gpt"
 
 func main() {
 	client, err := linebot.New(
@@ -53,164 +47,24 @@ func main() {
 			return
 		}
 		for _, event := range events {
-			if len(event.Source.GroupID) == 0 {
-				return
-			}
 			if event.Type == linebot.EventTypeMessage {
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
-					arr := strings.Split(message.Text, " ")
-					switch arr[0] {
-					case "喵嗚喵": // 取得訂閱者
-						var msg string
-						mu.Lock()
-						defer mu.Unlock()
-						userList, exists := subscribeMap[event.Source.GroupID]
-						if !exists || len(userList) == 0 {
-							msg = "目前沒有人需要提醒喵"
-						} else {
-							msg = "目前訂閱提醒服務的有:\n"
-							for _, user := range userList {
-								inHour := user.InHour + 8
-								if inHour >= 24 {
-									inHour -= 24
-								}
-								outHour := user.OutHour + 8
-								if outHour >= 24 {
-									outHour -= 24
-								}
-								msg += fmt.Sprintf("[%s] %02d:%02d - %02d:%02d\n", user.DisplayName, inHour, user.InMin, outHour, user.OutMin)
-							}
-							msg += "喵"
-						}
-						if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(msg)).Do(); err != nil {
-							log.Print(err)
-						}
-					case "喵嗚": // 訂閱
-						if len(arr) != 5 {
-							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("格式錯了喵")).Do(); err != nil {
-								log.Print(err)
-							}
-							return
-						}
-						userProfileResponse, err := bot.GetGroupMemberProfile(event.Source.GroupID, event.Source.UserID).Do()
-						if err != nil {
-							log.Print(err)
-							return
-						}
-						mu.Lock()
-						defer mu.Unlock()
-						_, exists := subscribeMap[event.Source.GroupID]
-						if !exists {
-							subscribeMap[event.Source.GroupID] = make(map[string]*User)
-						}
-						inHour, err := strconv.Atoi(arr[1])
-						if err != nil {
-							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("格式錯了喵")).Do(); err != nil {
-								log.Print(err)
-							}
-							return
-						}
-						inHour -= 8
-						if inHour < 0 {
-							inHour += 24
-						}
-						inMin, err := strconv.Atoi(arr[2])
-						if err != nil {
-							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("格式錯了喵")).Do(); err != nil {
-								log.Print(err)
-							}
-							return
-						}
-						outHour, err := strconv.Atoi(arr[3])
-						if err != nil {
-							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("格式錯了喵")).Do(); err != nil {
-								log.Print(err)
-							}
-							return
-						}
-						outHour -= 8
-						if outHour < 0 {
-							outHour += 24
-						}
-						outMin, err := strconv.Atoi(arr[4])
-						if err != nil {
-							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("格式錯了喵")).Do(); err != nil {
-								log.Print(err)
-							}
-							return
-						}
-						subscribeMap[event.Source.GroupID][event.Source.UserID] = &User{
-							DisplayName: userProfileResponse.DisplayName,
-							InHour:      inHour,
-							InMin:       inMin,
-							OutHour:     outHour,
-							OutMin:      outMin,
-						}
-						msg := userProfileResponse.DisplayName + " 已訂閱喵"
-						if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(msg)).Do(); err != nil {
-							log.Print(err)
-						}
-					case "喵喵": // 取消訂閱
-						userProfileResponse, err := bot.GetGroupMemberProfile(event.Source.GroupID, event.Source.UserID).Do()
-						if err != nil {
-							log.Print(err)
-							return
-						}
-						mu.Lock()
-						defer mu.Unlock()
-						_, exists := subscribeMap[event.Source.GroupID]
-						if !exists {
-							return
-						}
-						_, exists = subscribeMap[event.Source.GroupID][event.Source.UserID]
-						if !exists {
-							msg := userProfileResponse.DisplayName + " 你沒有訂閱喵"
-							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(msg)).Do(); err != nil {
-								log.Print(err)
-								return
-							}
-						}
-						delete(subscribeMap[event.Source.GroupID], event.Source.UserID)
-						msg := userProfileResponse.DisplayName + " 已取消訂閱喵"
-						if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(msg)).Do(); err != nil {
-							log.Print(err)
-						}
-					case "喵": // 打卡
-						userProfileResponse, err := bot.GetGroupMemberProfile(event.Source.GroupID, event.Source.UserID).Do()
-						if err != nil {
-							log.Print(err)
-							return
-						}
-						mu.Lock()
-						if user, exists := subscribeMap[event.Source.GroupID][event.Source.UserID]; exists {
-							now := time.Now()
-							if (now.Hour() == user.InHour && now.Minute()-15 <= user.InMin && now.Minute() >= user.InMin) ||
-								(now.Hour() == user.OutHour && now.Minute()-15 <= user.OutMin && now.Minute() >= user.OutMin) {
-								muok.Lock()
-								var msg string
-								if _, exists := ok[event.Source.GroupID]; !exists {
-									ok[event.Source.GroupID] = make(map[string]time.Time)
-								}
-								if _, exists := ok[event.Source.GroupID][event.Source.UserID]; exists {
-									msg = userProfileResponse.DisplayName + " 你打過卡了喵"
-								} else {
-									ok[event.Source.GroupID][event.Source.UserID] = time.Now().Add(time.Minute * 15)
-									msg = userProfileResponse.DisplayName + " 已打卡喵"
-								}
-								muok.Unlock()
-								if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(msg)).Do(); err != nil {
-									log.Print(err)
-								}
-							}
-						}
-						mu.Unlock()
+					// 呼叫 ChatGPT API 並取得回應
+					response, err := callChatGptAPI(message.Text)
+					if err != nil {
+						fmt.Println(err)
+						continue
+					}
+					// 回傳回應給使用者
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(response)).Do(); err != nil {
+						fmt.Println(err)
 					}
 				}
 			}
 		}
 	})
-	go run()
+
 	// This is just sample code.
 	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
 	if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -218,61 +72,30 @@ func main() {
 	}
 }
 
-var ok map[string]map[string]time.Time
-var muok sync.Mutex
-
-func run() {
-	for {
-		now := time.Now()
-		next := now.Add(time.Minute)
-		next = time.Date(next.Year(), next.Month(), next.Day(), next.Hour(), next.Minute(), 0, 0, next.Location())
-		t := time.NewTimer(next.Sub(now))
-		tt := <-t.C
-		if tt.Weekday() == 0 || tt.Weekday() == 6 {
-			continue
-		}
-		go func() {
-			muok.Lock()
-			for groupID := range ok {
-				for userID, t := range ok[groupID] {
-					if t.Before(tt) {
-						delete(ok[groupID], userID)
-					}
-				}
-			}
-			muok.Unlock()
-		}()
-		mu.Lock()
-		for groupID, userList := range subscribeMap {
-			var inMsg, outMsg string
-			for userID, user := range userList {
-				if _, exists := ok[groupID]; exists {
-					if _, exists := ok[groupID][userID]; exists {
-						continue
-					}
-				}
-				fmt.Println(tt)
-				fmt.Println(user)
-				if tt.Hour() == user.InHour && tt.Minute()-15 <= user.InMin && tt.Minute() >= user.InMin {
-					inMsg += user.DisplayName + ", "
-				}
-				if tt.Hour() == user.OutHour && tt.Minute()-15 <= user.OutMin && tt.Minute() >= user.OutMin {
-					outMsg += user.DisplayName + ", "
-				}
-			}
-			if len(inMsg) != 0 {
-				inMsg += "快點打卡上班喵！"
-				if _, err := bot.PushMessage(groupID, linebot.NewTextMessage(inMsg)).Do(); err != nil {
-					log.Print(err)
-				}
-			}
-			if len(outMsg) != 0 {
-				outMsg += "快點打卡下班喵！"
-				if _, err := bot.PushMessage(groupID, linebot.NewTextMessage(outMsg)).Do(); err != nil {
-					log.Print(err)
-				}
-			}
-		}
-		mu.Unlock()
+func callChatGptAPI(input string) (string, error) {
+	// 建立 HTTP 請求
+	req, err := http.NewRequest("POST", chatGptURL, strings.NewReader(`{
+        "model": "chatgpt","prompt": input,
+		"max_tokens": 64,
+		"temperature": 0.5,
+		"top_p": 1,
+		"frequency_penalty": 0,
+		"presence_penalty": 0
+	}`))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer 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")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
 	}
+	defer resp.Body.Close()
+
+	// 讀取回應內容
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
 }
